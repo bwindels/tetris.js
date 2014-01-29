082a140a49ab1e18a432843393d68dfbcd9f3079
Bruno Windels <bruno.windels@gmail.com>
2014-01-29 22:22:26 +0100

    initial (failing) unit test for TetrisField

---
+++ test/test-field.js
@@ -0,0 +1,28 @@
+var TetrisShape = require('../js/core/shape');
+var TetrisField = require('../js/core/field');
+
+module.exports = {
+    'test isShapeSpaceAvailable': function (test) {
+        
+        var s = new TetrisShape(4, 'red', [
+            [0, 0, 0, 0],
+            [0, 0, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0]
+        ]);
+        var f = new TetrisField(4, 6);
+
+        test.strictEqual(f.isShapeSpaceAvailable(0, 0, s), true);
+        test.strictEqual(f.isShapeSpaceAvailable(-1, -1, s), true);
+        test.strictEqual(f.isShapeSpaceAvailable(-2, -1, s), false);
+        test.strictEqual(f.isShapeSpaceAvailable(-1, -2, s), false);
+        
+        test.strictEqual(f.isShapeSpaceAvailable(1, 0, s), true);
+        test.strictEqual(f.isShapeSpaceAvailable(2, 0, s), false);
+        
+        test.strictEqual(f.isShapeSpaceAvailable(0, 3, s), true);
+        test.strictEqual(f.isShapeSpaceAvailable(0, 4, s), false);
+
+        test.done();
+    }
+};
